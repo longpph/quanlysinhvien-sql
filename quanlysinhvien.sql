0e49daec61514b00052dc3a12bd58f5087e34e20
longpph <long.pham2@student.codegym.vn>
2026-08-13 10:40:24 +0700

    update insert into

--- quanlysinhvien.sql
+++ quanlysinhvien.sql
@@ -35,3 +35,44 @@ create table Mark (
     foreign key (SubID) references Subject(SubID),
     foreign key (StudentID) references Student(StudentID)
 );
+
+insert into Class
+values (1, 'A1', '2008-12-20', 1);
+insert into Class
+values (2, 'A2', '2008-12-22', 1);
+insert into Class
+values (3, 'B3', current_date(), 0);
+
+
+insert into Student (StudentName, Address, Phone, Status, ClassId)
+values ('Hung', 'Ha Noi', '0912113113', 1, 1);
+insert into Student (StudentName, Address, Status, ClassId)
+values ('Hoa', 'Hai phong', 1, 1);
+insert into Student (StudentName, Address, Phone, Status, ClassId)
+values ('Manh', 'HCM', '0123123123', 0, 2);
+
+insert into Subject
+values (1, 'CF', 5, 1),
+ (2, 'C', 6, 1),
+ (3, 'HDJ', 5, 1),
+ (4, 'RDBMS', 10, 1);
+ 
+alter table Mark
+modify SubID int not null;
+ 
+drop table Mark;
+ 
+create table Mark (
+	MarkID int auto_increment primary key,
+    SubID int not null,
+    StudentID int not null,
+    Mark float default 0 check (Mark between 0 and 100),
+    ExamTime tinyint default 1,
+    foreign key (SubID) references Subject(SubID),
+    foreign key (StudentID) references Student(StudentID)
+);
+ 
+insert into Mark (SubId, StudentId, Mark, ExamTime)
+values (1, 1, 8, 1),
+ (1, 2, 10, 2),
+ (2, 1, 12, 1);
